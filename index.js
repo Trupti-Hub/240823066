97456e61a7fe9db452b3ca546223f9576070f4f2
atmiya <trupti.4267@gmail.com>
2025-07-15 11:04:54 +0530

    prg

---
+++ index.js
@@ -0,0 +1,31 @@
+// 1. Check Even or Odd 
+// Definition: Determine if a number is even or odd. 
+// Input: 7 
+// Output: Odd 
+
+
+// const a = 7
+
+// if(a%2 == 0)
+//     console.log("Even Number")
+// else
+//     console.log("Odd Number")
+
+// 2. Find the Maximum of Two Numbers 
+// Definition: Given two numbers, print the larger one. 
+// Input: 5 9 
+// Output: 9     
+
+// const a = 5
+// const b = 9
+
+// if(a > b)
+//     console.log(a,"is max")
+// else
+//     console.log(b,"is max")
+
+// 3. Check Leap Year 
+// Definition: Check if a given year is a leap year. 
+// Input: 2020 
+// Output: Leap Year 
+
